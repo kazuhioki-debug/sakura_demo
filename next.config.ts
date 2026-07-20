9a653cfe2101dd1000cdd8a6f95eb3c40a714ffc
kazuhioki-debug <kazuaki.hioki@gmail.com>
2026-07-20 23:28:27 +0900

    chore: setup github pages deploy workflow

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // 静的HTML書き出しを有効化
+  images: {
+    unoptimized: true, // サーバー不要で画像を表示するための設定
+  },
+  basePath: "/sakura_demo", // ★重要：GitHub Pagesのリポジトリ名「/sakura_demo」を指定
 };
 
 export default nextConfig;
